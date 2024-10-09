c2d874f090e14b0ef3a6a59956624ae68ae15de8
yasirukularathne <155828846+yasirukularathne@users.noreply.github.com>
2024-10-09 15:09:27 +0530

    updated bookstore

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+import PropTypes from 'prop-types'; // Step 1: Import PropTypes
+
+const BackButton = ({ destination = "/" }) => {
+  return (
+    <div className="flex">
+      <Link
+        to={destination}
+        className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
+      >
+        <BsArrowLeft className="text-2xl" />
+      </Link>
+    </div>
+  );
+};
+
+BackButton.propTypes = {
+  destination: PropTypes.string, // Step 2: Add propTypes validation
+};
+
+export default BackButton;
---
+++ frontend/src/components/home/BooksCards.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'; // Import PropTypes
+
+
+import BooksSingleCard from './BooksSingleCard';
+
+const BooksCard = ({ books }) => {
+  
+    return (
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {books.map((item) => (
+            <BooksSingleCard key={item._id} book={item} />
+          ))}
+        </div>
+  );
+};
+
+// PropTypes validation
+BooksCard.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      publishYear: PropTypes.number.isRequired, // Changed to number
+    })
+  ).isRequired,
+};
+
+export default BooksCard;
